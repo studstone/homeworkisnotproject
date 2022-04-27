2bc31f7b638208567df5462b41a8fa50bd870624
studstone <nikolas2996@gmail.com>
2022-04-27 10:33:40 +0300

    add class in main.js

--- main.js
+++ main.js
@@ -1,34 +1,17 @@
 'use strict';
 
-const DomElement = function (selector, height, width, bg, fontSize) {
-  this.selector = selector,
-    this.height = height,
-    this.width = width,
-    this.bg = bg,
-    this.fontSize = fontSize,
-    this.newElement = () => {
-      let div = {};
-      if (this.selector.startsWith('.', 0)) {
-        div = document.createElement('div');
-        div.classList = this.selector;
-        div.textContent = 'Господа, существующая теория обеспечивает широкому кругу (специалистов) участие в формировании анализа существующих паттернов поведения. А ещё предприниматели в сети интернет объективно рассмотрены соответствующими инстанциями.';
-      }
-      if (this.selector.startsWith('#', 0)) {
-        div = document.createElement('p');
-        div.id = this.selector;
-        div.textContent = 'Господа, постоянный количественный рост и сфера нашей активности, а также свежий взгляд на привычные вещи - безусловно открывает новые горизонты для модели развития. Но стремящиеся вытеснить традиционное производство, нанотехнологии освещают чрезвычайно интересные особенности картины в целом, однако конкретные выводы, разумеется, указаны как претенденты на роль ключевых факторов.';
-      }
-      div.style.cssText = `
-      height: ${this.height};
-      width: ${this.width};
-      background: ${this.bg};
-      font-size: ${this.fontSize};
-      `;
-      document.body.prepend(div);
-    };
+class First {
+  hello() {
+    console.log(`Привет я метод родителя!`);
+  }
+}
 
-};
+class Second extends First {
+  hello() {
+    super.hello();
+    console.log(`А я наследуемый метод!`);
+  }
+}
 
-const newDomElement = new DomElement();
-newDomElement.selector = '#block';
-newDomElement.newElement();
+const head = new Second();
+head.hello();
